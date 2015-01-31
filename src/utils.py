892e29b057322d241784519f42620b872d1d0e9f
Jason K. Moore <moorepants@gmail.com>
2015-01-30 16:08:50 -0800

    Added a configuration file parses and default config file.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# standard library
+import os
+
+# external
+import yaml
+
 from prettytable import PrettyTable
 
 
@@ -13,3 +19,34 @@ def print_gains(actual, *identified):
         table.add_column('Set {}'.format(i),
                          ['{:1.3f}'.format(g) for g in gain_set])
     print(table)
+
+
+def config_paths():
+    """Returns the full path to the directories specified in the config.yml
+    file.
+
+    Returns
+    -------
+    processed_dir : string
+        Absolute path to the processed data directory.
+
+    """
+
+    this_script_path = os.path.realpath(__file__)
+    src_dir = os.path.dirname(this_script_path)
+    root_dir = os.path.realpath(os.path.join(src_dir, '..'))
+
+    try:
+        with open(os.path.join(root_dir, 'config.yml'), 'r') as f:
+            config = yaml.load(f)
+    except IOError:
+        with open(os.path.join(root_dir, 'default-config.yml'), 'r') as f:
+            config = yaml.load(f)
+
+    processed_dir_name = os.path.join(root_dir, config['processed_data_dir'])
+    processed_dir = os.path.join(root_dir, processed_dir_name)
+
+    if not os.path.exists(processed_dir):
+        os.makedirs(processed_dir)
+
+    return processed_dir
