f9d39a82779a9360bbc439191d708c533079ebcf
Jason K. Moore <moorepants@gmail.com>
2015-02-05 12:50:39 -0800

    Added a bias vs noise ratio plot and made a heat map plot.

--- src/figure_bias.py
+++ src/figure_bias.py
@@ -12,6 +12,7 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 # this import is required for the projection='3d' to work
 from mpl_toolkits.mplot3d.axes3d import Axes3D
+from scipy.ndimage.filters import uniform_filter
 
 from model import QuietStandingModel
 from measured_data import DataGenerator
@@ -23,10 +24,10 @@ filename = os.path.join(utils.config_paths(), 'identified-gains.npz')
 if os.path.isfile(filename):
 
     with np.load(filename) as data:
-        ref_noise_stds = ['ref_noise_stds']
-        platform_pos_mags = ['platform_pos_mags']
+        ref_noise_stds = data['ref_noise_stds']
+        platform_pos_mags = data['platform_pos_mags']
         platform_acc_stds = data['platform_acc_stds']
-        identified_gains = ['identified_gains']
+        identified_gains = data['identified_gains']
 
 else:
 
@@ -71,7 +72,7 @@ else:
             platform_acc_stds[i, j] = data.measured['a'].std()
 
             direct_gains = direct_identification.identify(data.measured['u'],
-                                                        data.measured['x'])
+                                                          data.measured['x'])
             identified_gains[i, j, :] = direct_gains
 
             pbar.update(2 * i + j)
@@ -83,16 +84,81 @@ else:
                        platform_pos_mags=platform_pos_mags,
                        platform_acc_stds=platform_acc_stds)
 
+numerical_gains = np.array([[950.0, 175.0, 185.0, 50.0],
+                            [45.0, 290.0, 60.0, 26.0]]).flatten()
+
+mean_platform_acc_stds = platform_acc_stds.mean(axis=0)
+
 # Plot the results.
 
-fig, axes = plt.subplots(2, 4, subplot_kw={'projection': '3d'})
-axes = axes.flatten()
+fig_2d, axes_2d = plt.subplots(2, 4)
+axes_2d = axes_2d.flatten()
 
-# NOTE : Be careful with the 'indexing' option in meshgrid and make sure it
-# matches Z. The transpose below on identified_gains makes this work out.
-X, Y = np.meshgrid(ref_noise_stds, platform_acc_stds.mean(axis=0))
+fig_ratio, axes_ratio = plt.subplots(2, 4)
+axes_ratio = axes_ratio.flatten()
+
+###fig_3d, axes_3d = plt.subplots(2, 4, subplot_kw={'projection': '3d'})
+###axes_3d = axes_3d.flatten()
+###
+#### NOTE : Be careful with the 'indexing' option in meshgrid and make sure it
+#### matches Z. The transpose below on identified_gains makes this work out.
+###X, Y = np.meshgrid(ref_noise_stds, platform_acc_stds.mean(axis=0))
+
+# identified gains is [ref_idx, acc_idx, gain_idx]
+# identified gains.T is [gain_idx, acc_idx, ref_idx]
 
 for i, Z in enumerate(identified_gains.T):
-    axes[i].plot_surface(X[1:, :], Y[1:, :], Z[1:, :], cmap=cm.coolwarm,
-                         rstride=1, cstride=1, linewidth=0,
-                         antialiased=False)
+
+    # relative_diff is [acc_idx, ref_idx]
+
+    relative_diff = np.abs((Z[1:, :] - numerical_gains[i]) / numerical_gains[i])
+
+    relative_diff = uniform_filter(relative_diff, size=5, mode='constant')
+
+    num_ids = len(mean_platform_acc_stds) * len(ref_noise_stds)
+    ratios = np.empty(num_ids)
+    values = np.empty(num_ids)
+    for j, acc_row in enumerate(relative_diff):
+        ratios[j:j + len(ref_noise_stds)] = mean_platform_acc_stds[j] / ref_noise_stds
+        values[j:j + len(ref_noise_stds)] = acc_row
+
+    axes_ratio[i].plot(ratios[~np.isnan(ratios)] / 1000.0, values[~np.isnan(ratios)], '.')
+    axes_ratio[i].set_ylim((0.0, np.max(values[ratios > 200.0])))
+    if i > 3:
+        axes_ratio[i].set_xlabel('ACC / REF Noise / 1000')
+    if i == 0 or i == 4:
+        axes_ratio[i].set_ylabel('Relative error')
+
+    #print(np.max(relative_diff))
+
+    max_error = 0.5
+
+    relative_diff[relative_diff > max_error] = np.nan
+
+    # TODO : Ensure that each image is using the same color map range so
+    # that one colorbar can be used for all images.
+    image = axes_2d[i].imshow(relative_diff,
+                              cmap=cm.Reds,
+                              vmax=max_error,
+                              origin='lower')
+    axes_2d[i].set_title('Gain = {}'.format(numerical_gains[i]))
+    default_xticks = axes_2d[i].get_xticks()
+    x_labels = np.interp(default_xticks, np.arange(len(ref_noise_stds), dtype=float), ref_noise_stds)
+    axes_2d[i].set_xticklabels(['{:0.2f}'.format(r) for r in np.rad2deg(x_labels)])
+    if i > 3:
+        axes_2d[i].set_xlabel('Reference Noise STD [deg and deg/s]')
+    default_yticks = axes_2d[i].get_yticks()
+    y_labels = np.interp(default_yticks, np.arange(len(platform_acc_stds.mean(axis=0)), dtype=float), platform_acc_stds.mean(axis=0))
+    axes_2d[i].set_yticklabels(['{:0.2f}'.format(a) for a in y_labels])
+    if i == 0 or i == 4:
+        axes_2d[i].set_ylabel('Perturbation Acceleration STD [ms^-2]')
+
+    ###axes_3d[i].plot_surface(X[1:, :], Y[1:, :], relative_diff,
+                            ###cmap=cm.coolwarm, rstride=1, cstride=1,
+                            ###linewidth=0, antialiased=False)
+
+fig_2d.subplots_adjust(right=0.8)
+cax = fig_2d.add_axes([0.85, 0.1, 0.075, 0.8])
+plt.colorbar(mappable=image, cax=cax)
+
+plt.show()
