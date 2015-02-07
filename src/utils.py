08b686419a95a0d50f8bb61b66ea741e12e616eb
Jason K. Moore <moorepants@gmail.com>
2015-02-06 16:14:22 -0800

    Added a timeit decorator.

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,26 @@
 
 # standard library
 import os
+import time
 
 # external
 import yaml
 
 from prettytable import PrettyTable
+
+
+def timeit(method):
+
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print '%r (%r, %r) %2.2f sec' % \
+              (method.__name__, args, kw, te-ts)
+        return result
+
+    return timed
 
 
 def print_gains(actual, *identified):
